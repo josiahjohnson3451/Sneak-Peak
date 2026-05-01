3a82ee5153e87c3dfa8dbd37964e076c5e37f69b
Josiah Johnson <josiah.johnson@ascte.org>
2026-05-01 11:47:19 -0500

    fixed

---
+++ questions.js
@@ -0,0 +1,18 @@
+//Parts of a TSX file
+//Part 1: Import Area
+//Part 1.5: Global (within this file) section
+//Part 2: logic / function part where you write javascript
+function App(){
+
+    return (
+        // Part 3: UI area, whats displayed and looks like HTML
+    )
+}
+/*
+- What are the Questions YOU have?
+- What is VIte?
+- What is ChakraUI?
+- What are the 3 parts that makes up a Typescript file?
+- What is a component?
+- Add one ChkraUI Component to you app if you are already done
+*/
